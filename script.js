6a7b09b268525a5f50a48f5c7f3d721a89552680
Hoang-Son1 <hoangsony8driving@gmail.com>
2025-03-15 19:20:33 +1100

    Nav + timetable

---
+++ script.js
@@ -0,0 +1,77 @@
+// function k(){
+//     var x = document.getElementById("language")
+//     if (x.style.display == "none"){
+        
+//     }
+//     else{
+//     location.href ='entertainment.html'
+//     }
+// }
+function logo(){
+    location.href ='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+}
+function zoom(){
+    var x = document.getElementById("blank");
+    if (x.style.display == "flex"){
+        x.style.display = "none";
+    } else{
+        x.style.display = "flex";
+    }
+}
+function zoom2(){
+    var x = document.getElementById("blank2");
+    if (x.style.display == "flex"){
+        x.style.display = "none";
+    } else{
+        x.style.display = "flex";
+    }
+}
+function zoom3(){
+    var x = document.getElementById("blank3");
+    if (x.style.display == "flex"){
+        x.style.display = "none";
+    } else{
+        x.style.display = "flex";
+    }
+}
+function zoom4(){
+    var x = document.getElementById("blank4");
+    if (x.style.display == "flex"){
+        x.style.display = "none";
+    } else{
+        x.style.display = "flex";
+    }
+}
+function zoom5(){
+    var x = document.getElementById("blank5");
+    if (x.style.display == "flex"){
+        x.style.display = "none";
+    } else{
+        x.style.display = "flex";
+    }
+}
+function zoom6(){
+    var x = document.getElementById("blank6");
+    if (x.style.display == "flex"){
+        x.style.display = "none";
+    } else{
+        x.style.display = "flex";
+    }
+}
+function zoom7(){
+    var x = document.getElementById("blank7");
+    if (x.style.display == "flex"){
+        x.style.display = "none";
+    } else{
+        x.style.display = "flex";
+    }
+}
+function zoom8(){
+    var x = document.getElementById("blank8");
+    if (x.style.display == "flex"){
+        x.style.display = "none";
+    } else{
+        x.style.display = "flex";
+    }
+}
+
